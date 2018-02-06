a44d384023921d5fca84de188d1e0cc903e551f1
jaenuguid <jaenuguid@jae-pc>
2018-02-06 23:47:58 +0800

    Added uml

--- Observer design pattern/IObservable.java
+++ Observer design pattern/IObservable.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 public interface IObservable {
 
 
-	ArrayList<IObserver> observers  = new ArrayList<IObserver>();
-	
 	public void add(IObserver observer);
 	public void remove(String observerName);
 	public void notifyObservers();
